cddcf2a9e2abae2599b5343dac610b1e288ce52a
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2016-06-15 16:20:38 +0200

    Make the json compliant
    + Refactor

--- rrd/export-rrd.py
+++ rrd/export-rrd.py
@@ -5,12 +5,14 @@
 """
 
 import click
+import json
 import os
 import subprocess
 
+
 DIRPATH='/var/lib/munin/softwareheritage.org/'
 FILENAME_PATTERN="prado.softwareheritage.org-softwareheritage_objects_softwareheritage-###-g.rrd"
-# The data source used to create the rrd file
+# The data source used at rrd creation time
 DS=42
 
 ENTITIES=[
@@ -31,9 +33,14 @@ ENTITIES=[
     "visit",
 ]
 
-@click.command()
-@click.option('--dirpath', default=DIRPATH)
-def main(dirpath):
+
+def compute_cmd(dirpath):
+    """Compute the command to execute to retrieve the needed data.
+
+    Returns:
+        The command as string.
+
+    """
     cmd=''
     for entity in ENTITIES:
         filename = FILENAME_PATTERN.replace('###', entity)
@@ -45,9 +52,45 @@ def main(dirpath):
                  entity, filepath, DS, entity, entity,
                  entity, filepath, DS, entity, entity)
 
-    run_cmd = 'rrdtool xport --json --start 1431436580 --step 86400 %s' % (cmd, )
+    return 'rrdtool xport --json --start 1431436580 --step 86400 %s' % (cmd, )
 
-    subprocess.check_call(run_cmd, shell=True)
+
+def retrieve_json(cmd):
+    """Given the cmd command, execute and returns the right json format.
+
+    Args:
+        cmd: the command to execute to retrieve the desired json.
+
+    Returns:
+        The desired result as json string.
+    """
+    cmdpipe = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+    data = ''
+    while True:
+        line = cmdpipe.stdout.readline()
+        if not line:
+            break
+        # Hack: the json outputd is not well-formed...
+        line = line.replace('\'', '"')
+        line = line.replace('about: ', '"about": ')
+        line = line.replace('meta:', '"meta": ')
+        data += line
+
+    cmdpipe.stdout.close()
+    return data
+
+
+@click.command()
+@click.option('--dirpath', default=DIRPATH)
+def main(dirpath):
+    # Delegate the execution to the system
+    run_cmd = compute_cmd(dirpath)
+    json_data = retrieve_json(run_cmd)
+
+    # to make sure the json is well-formed
+    data = json.loads(json_data)
+    # TODO: prepare the json appropriately here
+    print(json.dumps(data))
 
 
 if __name__ == '__main__':
