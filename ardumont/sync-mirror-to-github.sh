9bf26570977292588750c42a5028e66e046c7186
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2017-02-03 08:58:05 +0100

    mirroring script: Improve default behavior
    - Retrieve the list of urls already set, if github is already set,
      consider the mirror done and stops.
    
    - Since all repositories are not configured to have a shortname,
      extract a 'display' uri to extract a correct shortname for the
      repository (ex: swh-indexer, swh-loader-git, etc...). This will
      permit to mirror with the same shortname in github our forge
      repositories. Also this removes the shortname parameter of the
      script.
    
    --
    
    Ideally, it'd be awesome if the other information could be retrieved
    as well:
    - repo's uri: there are at least 3 different uris in the
      forge ('display', 'raw', etc... which one do i choose?)
    - repo's description (did not find it yet in the api call output),
    - etc...
    
    This script would only then have one parameter as input (at the
    moment, the callsign).

--- ardumont/sync-mirror-to-github.sh
+++ ardumont/sync-mirror-to-github.sh
@@ -20,10 +20,9 @@ Install the forge's token in $FORGE_API_TOKEN_FILE
 Once the installation is done, you can trigger this script again.
 EOF
 
-[[ ( $# -lt 4 ) ]] && cat << EOF && exit 1
+[[ ( $# -lt 3 ) ]] && cat << EOF && exit 1
 Use: $0 <FORGE_REPO_CALLSIGN> <FORGE_REPO_NAME> <FORGE_REPO_URL> <FORGE_REPO_DESCRIPTION>
 FORGE_REPO_CALLSIGN      Repository's callsign (we will deduce the phid from it)
-FORGE_REPO_NAME          Repository's name (we will use it for github name)
 FORGE_REPO_URL           Repository's forge url
 FORGE_REPO_DESCRIPTION   Repository's forge description
 CREDENTIAL_KEY_ID        Credential key id to use (default to 2 which is K2)
@@ -34,10 +33,9 @@ sync-mirror-to-github.sh swh-indexer https://forge.softwareheritage.org/source/s
 EOF
 
 FORGE_REPO_CALLSIGN=$1
-FORGE_REPO_NAME=$2
-FORGE_REPO_URL=$3
-FORGE_REPO_DESCRIPTION=$4
-CREDENTIAL_KEY_ID=${5-"2"}  # default to K2
+FORGE_REPO_URL=$2
+FORGE_REPO_DESCRIPTION=$3
+CREDENTIAL_KEY_ID=${4-"2"}  # default to K2
 
 #### Retrieve phabricator information
 
@@ -45,10 +43,28 @@ FORGE_API_TOKEN=$(cat $FORGE_API_TOKEN_FILE)
 
 DATA_REPO=$(curl -d api.token=$FORGE_API_TOKEN \
                  -d "constraints[callsigns][0]=$FORGE_REPO_CALLSIGN" \
+                 -d "attachments[uris]=true" \
                  https://forge.softwareheritage.org/api/diffusion.repository.search 2>/dev/null)
 
 # Retrieve the repository's phid
 FORGE_REPO_PHID=$(echo $DATA_REPO | jq '.result.data[].phid' | sed -e 's/"//gi')
+
+FORGE_REPO_URLS=$(echo $DATA_REPO | jq '.result.data[].attachments.uris.uris[].fields.uri.display' \
+                      | sed -e 's/"//gi')
+
+already_mirrored=$(echo $FORGE_REPO_URLS | grep 'github')
+
+if [ ! -z "$already_mirrored" ]; then
+    echo "Repository $FORGE_REPO_CALLSIGN already mirrored. Skipping."
+    exit 1
+fi
+
+# Retrieve name from url
+mirror_url=$(echo $DATA_REPO | jq '.result.data[].attachments.uris.uris[].fields.uri.display' \
+                 | sed -e 's/"//gi' | head -1 | xargs basename)
+
+repo_name=${mirror_url%%.git}
+
 
 #### Create the repository in github
 
@@ -57,7 +73,7 @@ GITHUB_USER_TOKEN=$(cat $GITHUB_API_TOKEN_FILE)
 TMP_FILE=$(mktemp)
 cat <<EOF > $TMP_FILE
 {
-  "name": "$FORGE_REPO_NAME",
+  "name": "$repo_name",
   "description": "$FORGE_REPO_DESCRIPTION",
   "homepage": "$FORGE_REPO_URL",
   "private": false,
@@ -78,7 +94,7 @@ curl -H "Authorization: token $GITHUB_USER_TOKEN" \
 
 #### phabricator
 
-GITHUB_REPO_URI=git@github.com:SoftwareHeritage/$FORGE_REPO_NAME.git
+GITHUB_REPO_URI=git@github.com:SoftwareHeritage/$repo_name.git
 
 KEY_PHID=$(curl -d "api.token=$FORGE_API_TOKEN" \
                 -d "ids[0]=$CREDENTIAL_KEY_ID" \
