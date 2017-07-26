714d2eb262841285db36a73692900249c0265400
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2017-07-26 12:54:07 +0200

    Group exception error by origin types

--- ardumont/group_by_exception.py
+++ ardumont/group_by_exception.py
@@ -6,35 +6,66 @@
 
 # Use:
 # ./kibana_fetch_logs.py | tee temporary-error-file | \
-#    ./group_by_exception.py > temporary-error-file-groupby-exception
+#    ./group_by_exception.py | jq > temporary-error-file-groupby-exception
 
 import ast
 import click
+import json
+import operator
 import sys
 
-from collections import defaultdict
+from collections import defaultdict, OrderedDict
 
 
 def work_on_exception_msg(exception):
-    return exception[0:30]
+    return exception[0:50]
+
+
+def group_by(origin_types):
+    group = {ori_type: defaultdict(list) for ori_type in origin_types}
+
+    for line in sys.stdin:
+        origin_type = None
+        line = line.strip()
+        data = ast.literal_eval(line)
+        for ori_type in origin_types:
+            if ori_type in data['args']['origin_url']:
+                origin_type = ori_type
+                break
+
+        if not origin_type:
+            continue
+
+        reworked_exception_msg = work_on_exception_msg(data['exception'])
+        group[origin_type][reworked_exception_msg].append(data['args'])
+
+    return group
 
 
 @click.command()
-@click.option('--file', help='File to read data from')
-def main(file):
-    group = defaultdict(list)
-
-    for line in sys.stdin:
-        line = line.strip()
-        data = ast.literal_eval(line)
-        reworked_exception_msg = work_on_exception_msg(data['exception'])
-        group[reworked_exception_msg].append(data['args'])
+@click.option('--origin-types', default=['gitorious', 'googlecode'],
+              help='Default types of origin to lookup')
+def main(origin_types):
+    group = group_by(origin_types)
 
     result = {}
-    for k, v in group.items():
-        result[k] = len(v)
+    for ori_type in origin_types:
+        _map = {}
+        total = 0
+        for k, v in group[ori_type].items():
+            l = len(v)
+            _map[k] = l
+            total += l
 
-    print(result)
+        out = sorted(_map.items(), key=operator.itemgetter(1),
+                     reverse=True)
+
+        result[ori_type] = {
+            'total': total,
+            'errors': OrderedDict(out),
+        }
+
+    print(json.dumps(result))
 
 
 if __name__ == '__main__':
