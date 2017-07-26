8c78c2e0558d1e6b284f7053d8ef7e4cb07fc453
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2017-07-26 11:26:31 +0200

    Add snippet script to retrieve error log msgs from kibana instance

---
+++ ardumont/kibana_fetch_logs.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python3
+# Use:
+# ./kibana_fetch_logs.py | tee temporary-error-file
+
+# Copyright (C) 2017 the Software Heritage developers
+# License: GNU General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+
+import ast
+import click
+import requests
+
+
+PAYLOAD_REQUESTS = {
+  "from": 0,
+  "_source": [
+    "swh_logging_args_exc",
+    "swh_logging_args_kwargs"
+  ],
+  "sort": [
+    {
+      "@timestamp": "asc"
+    }
+  ],
+  "query": {
+    "bool": {
+      "must": [
+        {
+          "match": {
+            "systemd_unit": {
+              "query": "swh-worker@swh_loader_git_disk.service",
+              "type": "phrase"
+            }
+          }
+        },
+        {
+          "term": {
+            "priority": "3"
+          }
+        }
+      ],
+      "must_not": [
+        {
+          "match": {
+            "message": {
+              "query": "[.*] consumer: Cannot connect to amqp.*",
+              "type": "phrase"
+            }
+          }
+        },
+        {
+          "match": {
+            "message": {
+              "query": "[.*] pidbox command error.*",
+              "type": "phrase"
+            }
+          }
+        }
+      ]
+    }
+  }
+}
+
+
+def retrieve_data(server, indexes, types, size, start=None):
+    """Retrieve information from server looking up through 'indexes' and
+    'types'.  This returns result of length 'size' starting from the
+    'start' position.
+
+    """
+    payload = PAYLOAD_REQUESTS.copy()
+    payload['size'] = size
+    if start:
+        payload['search_after'] = [start]
+
+    url = '%s/%s/%s/_search' % (server, indexes, types)
+    r = requests.post(url, json=payload)
+    if r.ok:
+        return r.json()
+
+
+def format_result(json):
+    """Format result from the server's response"""
+    all_data = []
+    total_hits = json['hits']['total']
+    for data in json['hits']['hits']:
+        last_sort_time = data['sort'][0]
+        source = data['_source']
+        all_data.append({
+            'args': ast.literal_eval(source['swh_logging_args_kwargs']),
+            'exception': source['swh_logging_args_exc']
+        })
+
+    return all_data, last_sort_time, total_hits
+
+
+def query_log_server(server, indexes, types, size):
+    count = 0
+    last_sort_time = None
+    total_hits = 1
+
+    while count < total_hits:
+        response = retrieve_data(server, indexes, types, size,
+                                 start=last_sort_time)
+        all_data, last_sort_time, total_hits = format_result(response)
+
+        for row in all_data:
+            count += 1
+            yield row
+
+
+@click.command()
+@click.option('--server',
+              default='http://banco.internal.softwareheritage.org:9200',
+              help='Elastic search instance to query against')
+@click.option('--indexes',
+              default='logstash-2017.05.*,logstash-2017.06.*',
+              help='ElasticSearch indexes to lookup (csv if many)')
+@click.option('--types', default='journal',
+              help='ElasticSearch types to lookup (csv if many)')
+@click.option('--size', default=10, type=click.INT, help='Pagination size')
+def main(server, indexes, types, size):
+    for entry in query_log_server(server, indexes, types, size):
+        print(entry)
+
+
+if __name__ == '__main__':
+    main()
