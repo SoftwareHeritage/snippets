20cb59c349bc217f82b14653f28039ab284b8c89
Stefano Zacchiroli <zack@upsilon.cc>
2018-09-19 17:13:29 +0200

    dump-swh-graph.sql: add support to dump snapshot table

--- sql/swh-graph/dump-swh-graph.sql
+++ sql/swh-graph/dump-swh-graph.sql
@@ -2,6 +2,12 @@
 \copy directory_entry_dir (id, name, target) to program 'pigz -c > directory_entry_dir.csv.gz' (format csv);
 \copy directory_entry_file (id, name, target) to program 'pigz -c > directory_entry_file.csv.gz' (format csv);
 \copy directory_entry_rev (id, name, target) to program 'pigz -c > directory_entry_rev.csv.gz' (format csv);
+
 \copy revision (id, directory, object_id) to program 'pigz -c > revision.csv.gz' (format csv);
 \copy revision_history (id, parent_id, parent_rank) to program 'pigz -c > revision_history.csv.gz' (format csv);
+
 \copy release (id, target, target_type, object_id) to program 'pigz -c > release.csv.gz' (format csv);
+
+\copy snapshot (id, object_id) to program 'pigz -c > snapshot.csv.gz' (format csv);
+\copy snapshot_branches (snapshot_id, branch_id) to program 'pigz -c > snapshot_branches.csv.gz' (format csv);
+\copy snapshot_branch (object_id, name, target, target_type) to program 'pigz -c > snapshot_branch.csv.gz' (format csv);
