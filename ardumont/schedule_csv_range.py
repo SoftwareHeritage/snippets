4248aa07a75cb56786bcdce095201411bf855d37
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2018-11-22 17:15:17 +0100

    schedule_csv_range: Add task_type and policy

--- ardumont/schedule_csv_range.py
+++ ardumont/schedule_csv_range.py
@@ -2,7 +2,11 @@ import click
 
 
 @click.command()
-def main(limit):
+@click.option('--task-type', '-t', default='indexer_mimetype_range',
+              help='Add task type')
+@click.option('--policy', '-p', default='oneshot',
+              help='Task policy, either oneshot or recurring')
+def main(task_type, policy):
     # sha1 range
     # 160 bits (20 byte) length
     bound_max_minus = 2**159
@@ -16,7 +20,7 @@ def main(limit):
             range(0, bound_max_minus, step), range(step, bound_max, step)):
         hex_start = to_hex(start)
         hex_end = to_hex(end)
-        print('["%s", "%s"]' % (hex_start, hex_end))
+        print('%s;%s,["%s", "%s"]' % (task_type, policy, hex_start, hex_end))
 
 
 if __name__ == '__main__':
