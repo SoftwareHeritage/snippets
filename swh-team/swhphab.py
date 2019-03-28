b1f0b7ba0c6f057722c8a2800e2698bf2f758c67
Stefano Zacchiroli <zack@upsilon.cc>
2019-03-28 21:58:17 +0100

    swh-weekly-report: preserve iterators and port to current Phabricator

--- swh-team/swhphab.py
+++ swh-team/swhphab.py
@@ -6,22 +6,19 @@ def paginate(query, args, stop):
     """perform a query paginating through results until stop condition is met
 
     """
-    def do_query():
-        after = '0'
-        keep_going = True
-        while keep_going:
-            r = query(**args, after=after)
-            if not(r['data']):
+    after = None
+    keep_going = True
+    while keep_going:
+        r = query(**args, after=after)
+        if not(r['data']):
+            break
+        for item in r['data']:
+            if stop(item):
+                keep_going = False
                 break
-            for item in r['data']:
-                if stop(item):
-                    keep_going = False
-                    break
-                yield item
+            yield item
 
-            after = r['cursor']['after']
-
-    return list(do_query())
+        after = r['cursor']['after']
 
 
 @lru_cache()
