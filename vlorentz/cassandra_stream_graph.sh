effc91b60be6fb11c2a75b6ce3bd6679eaa6c4ca
Valentin Lorentz <vlorentz@softwareheritage.org>
2019-11-08 13:07:45 +0100

    Export all tables and compress outputs.

--- vlorentz/cassandra_stream_graph.sh
+++ vlorentz/cassandra_stream_graph.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+PYTHON=python3
+NB_PARTITIONS=16
+TABLES="content directory directory_entry revision release snapshot snapshot_branch origin_visit origin"
+TABLES="revision"
+
 tmp_dir=$(mktemp -td swh-graph-export.XXXXXXXXXX)
 trap "rm -rf ${tmp_dir}; pkill -P $$" EXIT
 
@@ -10,13 +15,20 @@ edges_fifo="${tmp_dir}/edges.csv.fifo"
 mkfifo "${nodes_fifo}"
 mkfifo "${edges_fifo}"
 
-NB_PARTITIONS=16
-for ((partition_id=0;partition_id<NB_PARTITIONS;partition_id++)); do
-    python3 cassandra_stream_graph.py "${nodes_fifo}" "${edges_fifo}" --table revision --nb-partitions "${NB_PARTITIONS}" --partition-id "${partition_id}" &
+
+cat "${nodes_fifo}" | pigz -c > nodes.csv.gz &
+cat "${edges_fifo}" | pigz -c > edges.csv.gz &
+
+for table in ${TABLES}; do
+    echo "Exporting ${table}"
+    pids=""
+    for ((partition_id=0;partition_id<NB_PARTITIONS;partition_id++)); do
+        ${PYTHON} cassandra_stream_graph.py "${nodes_fifo}" "${edges_fifo}" --table "${table}" --nb-partitions "${NB_PARTITIONS}" --partition-id "${partition_id}" &
+        pids+=($!)
+    done
+    for pid in ${pids[@]}; do
+        wait "${pid}"
+    done
 done
-
-
-cat "${nodes_fifo}" > nodes.csv &
-cat "${edges_fifo}" > edges.csv &
 
 wait
