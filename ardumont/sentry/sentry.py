24905d9329b32a471b15d4235a1243ffde815ae4
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2020-03-20 15:51:13 +0100

    sentry: Add copyright header

--- ardumont/sentry/sentry.py
+++ ardumont/sentry/sentry.py
@@ -1,4 +1,7 @@
-#!/usr/bin/env python3
+# Copyright (C) 2020 The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU General Public License version 3, or any later version
+# See top-level LICENSE file for more information
 
 import json
 import logging
