e9a26ada45c88ed93a732ded8628255e75a7e7e6
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2020-08-08 20:06:27 +0200

    ardumont/schedule_csv_partition.py
    Related to scheduling of new indexer tasks

--- ardumont/schedule_csv_partition.py
+++ ardumont/schedule_csv_partition.py
@@ -10,13 +10,17 @@ import click
 import random
 
 
+def _is_power_of_two(n: int) -> bool:
+    return n > 0 and n & (n - 1) == 0
+
+
 @click.command()
 @click.option('--task-type', '-t', default='index-mimetype-partition',
               help='Add task type')
 @click.option('--policy', '-p', default='oneshot',
               help='Task policy, either oneshot or recurring')
-@click.option('--number-partitions', '-n', default=100000,
-              help='Number of ranges to compute')
+@click.option('--number-partitions', '-n', default=1024,
+              help='(A power of 2) number of partitions to compute')
 def main(task_type, policy, number_partitions):
     """Compute `number-partitions` for a given `task-type` with a given `policy`.
 
@@ -24,17 +28,21 @@ def main(task_type, policy, number_partitions):
     $ python3 -m schedule_csv_partition \
               --task-type index-mimetype-partition \
               --policy recurring \
-              --number-ranges 100000 | head -2
-    index-mimetype-partition;oneshot;[30936, 100000]
-    index-mimetype-partition;oneshot;[72794, 100000]
+              --number-ranges 1024 | head -2
+    index-mimetype-partition;oneshot;[122, 1024]
+    index-mimetype-partition;oneshot;[720, 1024]
 
     Schedule the tasks (providing file.csv contains the result of the previous
     command):
 
-    $ head -1 file.csv | python3 -m swh.scheduler.cli task schedule \
+    $ head -1 file.csv | swh scheduler task schedule \
         -c type -c policy -c args --delimiter ';' -
 
     """
+    if not _is_power_of_two(number_partitions):
+        # expected by the storage
+        raise ValueError("Number of partitions must be a power of 2")
+
     partition_ids = [partition_id for partition_id in range(number_partitions)]
     random.shuffle(partition_ids)
 
