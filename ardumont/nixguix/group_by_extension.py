8dc4b0306a9d813a4fd9e9b55292c78a28e097d6
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2020-08-08 20:14:34 +0200

    group_by_extension: Sort output with order desc
    Also fallbacks on keying on basename when no extension is detected (for crates
    for example...)

--- ardumont/nixguix/group_by_extension.py
+++ ardumont/nixguix/group_by_extension.py
@@ -9,20 +9,23 @@
 # {
 #   ".tar.gz": 308,
 #   ".tar.xz": 152,
+#   "download": 102,
 #   ".tar.bz2": 79,
+#   ".tbz": 79,
+#   ".tgz": 40,
+#   ".zip": 23,
 #   ".tar.lz": 6,
 #   ".lisp": 1,
-#   ".tgz": 40,
-#   "no-extension": 106,
 #   ".1.0": 1,
+#   "": 1,
 #   ".el?id=dcc9ba03252ee5d39e03bba31b420e0708c3ba0c": 1,
 #   ".11": 1,
 #   ".tar.z": 1,
 #   ".1.5": 1,
 #   ".4.2": 1,
 #   ".sf3": 1,
-#   ".zip": 23,
 #   ".7z": 1,
+#   "21": 1,
 #   ".install.gz": 1,
 #   ".gz": 1,
 #   ".tar.lzma": 1,
@@ -31,17 +34,20 @@
 #   ".tar.Z": 1,
 #   ".1": 1,
 #   ".py": 1,
-#   ".tbz": 79,
+#   "use_minizip": 1,
+#   "?p=mmh;a=snapshot;h=431604647f89d5aac7b199a7883e98e56e4ccf9e;sf=tgz": 1,
 #   ".8.3;sf=tgz": 1,
 #   ".git;a=snapshot;h=V7_3_0p3;sf=tgz": 1,
 #   ".0-tar.gz": 1,
 #   ".tar": 1
 # }
 
-import os
-import json
 
-from collections import defaultdict
+import json
+import operator
+import os
+
+from collections import defaultdict, OrderedDict
 from typing import Dict, List
 
 import click
@@ -63,7 +69,8 @@ def main(file_with_failures):
     for url in data:
         prefix, ext = os.path.splitext(url)
         if not ext:
-            url_per_extension["no-extension"].append(url)
+            # fallback on using the basename of the url
+            url_per_extension[os.path.basename(url)].append(url)
             continue
 
         if ext in known_single_extension:  # known unique extension
@@ -78,9 +85,14 @@ def main(file_with_failures):
         # etension is double e.g. .tar.gz, etc...
         url_per_extension[f"{ext2}{ext}"].append(url)
 
+    # Simplify the following into less steps
     summary: Dict[str, int] = {}
     for ext, urls in url_per_extension.items():
         summary[ext] = len(urls)
+
+    out = sorted(summary.items(), key=operator.itemgetter(1),
+                 reverse=True)
+    summary = OrderedDict(out)
 
     print(json.dumps(summary))
 
