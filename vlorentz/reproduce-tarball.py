f26d2051b1a42a0487a5bbb62e71eb3bf0330b05
Valentin Lorentz <vlorentz@softwareheritage.org>
2020-08-28 22:10:59 +0200

    reproduce-tarball: add support for disarchive, as an alternative to pristine-tar.

--- vlorentz/reproduce-tarball.py
+++ vlorentz/reproduce-tarball.py
@@ -84,8 +84,80 @@ def revision_swhid_from_status(storage, status):
     return SWHID(object_type="revision", object_id=hash_to_hex(revision_id))
 
 
+def pristine_gendelta(source_path):
+    if source_path.endswith((".tar.gz", ".tar.bz2", ".tar.xz", ".tgz", ".tbz2")):
+        type_ = "tar"
+        extra_options = []
+    elif source_path.endswith((".zip",)):
+        type_ = "zip"
+        extra_options = ["--lax-guess"]
+    else:
+        assert False, source_path
+
+    with tempfile.NamedTemporaryFile() as delta_fd:
+        proc = subprocess.run(
+            [
+                "pristine-" + type_,
+                *extra_options,
+                "gendelta",
+                source_path,
+                delta_fd.name,
+            ],
+            capture_output=True,
+        )
+        assert proc.returncode == 0, proc
+        pristine_delta = delta_fd.read()
+        overhead_size = len(pristine_delta)
+
+    pristine = {b"type": type_, b"delta": pristine_delta}
+
+    tar_metadata = {
+        b"filename": os.path.basename(source_path),
+        b"pristine": pristine,
+    }
+
+    return (tar_metadata, overhead_size)
+
+
+def disarchive_save(source_path):
+    with tempfile.TemporaryDirectory(suffix="_src_dirdb") as dirdb:
+        proc = subprocess.run(["sha256sum", source_path], capture_output=True)
+        assert proc.returncode == 0
+        source_sha256 = proc.stdout.decode().strip().split()[0]
+        with tempfile.TemporaryDirectory(suffix="src_dircache") as dircache:
+            proc = subprocess.run(
+                ["disarchive", "save", source_path],
+                env={
+                    **os.environ,
+                    "DISARCHIVE_DB": dirdb,
+                    "DISARCHIVE_DIRCACHE": dircache,
+                },
+                capture_output=True,
+            )
+            dircache_dirs = os.listdir(os.path.join(dircache, "sha256"))
+            (dircache_sha256,) = dircache_dirs
+        assert proc.returncode == 0, proc
+        with tempfile.NamedTemporaryFile(suffix=".tar.gz") as disarchive_db_fd:
+            proc = subprocess.run(
+                ["tar", "-cz", ".", "-f", disarchive_db_fd.name],
+                cwd=dirdb,
+                capture_output=True,
+            )
+            assert proc.returncode == 0, proc
+            disarchive_db = disarchive_db_fd.read()
+        overhead_size = len(disarchive_db)
+
+    tar_metadata = {
+        b"filename": os.path.basename(source_path),
+        b"sha256": source_sha256,
+        b"disarchive": {b"dircache_sha256": dircache_sha256, b"disarchive_db": disarchive_db},
+    }
+
+    return (tar_metadata, overhead_size)
+
+
 def ingest_tarball(
-    storage: StorageInterface, source_path: str, *, verbose: bool
+    method, storage: StorageInterface, source_path: str, *, verbose: bool
 ) -> Sha1Git:
     """Ingests a tarball in the storage, returns the revision's SWHID"""
     url = "file://" + os.path.abspath(source_path)
@@ -113,31 +185,13 @@ def ingest_tarball(
     storage.metadata_authority_add([AUTHORITY])
     storage.metadata_fetcher_add([FETCHER])
 
-    if source_path.endswith((".tar.gz", ".tar.bz2", ".tar.xz", ".tgz", ".tbz2")):
-        type_ = "tar"
-        extra_options = []
-    elif source_path.endswith((".zip",)):
-        type_ = "zip"
-        extra_options = ["--lax-guess"]
+    if method == "pristine":
+        (tar_metadata, overhead_size) = pristine_gendelta(source_path)
     else:
-        assert False, source_path
+        (tar_metadata, overhead_size) = disarchive_save(source_path)
 
-    with tempfile.NamedTemporaryFile() as delta_fd:
-        proc = subprocess.run(
-            ["pristine-" + type_, *extra_options, "gendelta", source_path, delta_fd.name],
-            capture_output=True,
-        )
-        assert proc.returncode == 0, proc
-        pristine_delta = delta_fd.read()
     if verbose:
-        print(f"Size of pristine delta: {len(pristine_delta)} bytes")
-
-    pristine = {b"type": type_, b"delta": pristine_delta}
-
-    tar_metadata = {
-        b"filename": os.path.basename(source_path),
-        b"pristine": pristine,
-    }
+        print(f"Tarball overhead: {overhead_size} bytes")
 
     storage.raw_extrinsic_metadata_add(
         [
@@ -147,17 +201,19 @@ def ingest_tarball(
                 discovery_date=datetime.datetime.now(tz=datetime.timezone.utc),
                 authority=AUTHORITY,
                 fetcher=FETCHER,
-                format="tarball-metadata-msgpack",
+                format=f"tarball-metadata-msgpack-{method}",
                 metadata=msgpack.dumps(tar_metadata),
             )
         ]
     )
 
-    return (revision_swhid, len(pristine_delta))
+    return (revision_swhid, overhead_size)
 
 
 def get_tar_metadata(
-    storage: StorageInterface, revision_swhid: SWHID,
+    method: str,
+    storage: StorageInterface,
+    revision_swhid: SWHID,
 ):
     """Return the stored metadata for the given revision."""
     metadata = stream_results(
@@ -172,12 +228,12 @@ def get_tar_metadata(
             last_metadata = item
     assert last_metadata is not None
 
-    assert last_metadata.format == "tarball-metadata-msgpack"
+    assert last_metadata.format == f"tarball-metadata-msgpack-{method}"
 
     return msgpack.loads(last_metadata.metadata)
 
 
-def generate_tarball(
+def generate_tarball_pristine(
     storage: StorageInterface,
     revision_swhid: SWHID,
     target_path: str,
@@ -186,7 +242,7 @@ def generate_tarball(
 ):
     """Using only the storage and revision SWHID, regenerates a tarball
     identical to the source tarball."""
-    tar_metadata = get_tar_metadata(storage, revision_swhid)
+    tar_metadata = get_tar_metadata("pristine", storage, revision_swhid)
 
     revision_id = hash_to_bytes(revision_swhid.object_id)
     revision = list(storage.revision_get([revision_id]))[0]
@@ -195,7 +251,7 @@ def generate_tarball(
         dir_builder = DirectoryBuilder(storage, tempdir.encode(), revision["directory"])
         dir_builder.build()
 
-        type_ = tar_metadata[b"pristine"][b"type"].decode()
+        type_ = tar_metadata[b"pristine"][b"type"]
         assert type_ in ("tar", "zip"), f"Unknown type {type_}"
 
         # pristine-tar needs a different CWD depending on the number of root
@@ -222,6 +278,61 @@ def generate_tarball(
     return proc.returncode == 0
 
 
+def generate_tarball_disarchive(
+    storage: StorageInterface,
+    revision_swhid: SWHID,
+    target_path: str,
+    *,
+    verbose: bool,
+):
+    """Using only the storage and revision SWHID, regenerates a tarball
+    identical to the source tarball."""
+    tar_metadata = get_tar_metadata("disarchive", storage, revision_swhid)
+
+    revision_id = hash_to_bytes(revision_swhid.object_id)
+    revision = list(storage.revision_get([revision_id]))[0]
+
+    source_sha256 = tar_metadata[b"sha256"]
+    dircache_sha256 = tar_metadata[b"disarchive"][b"dircache_sha256"]
+
+    with tempfile.TemporaryDirectory(suffix="_dest_dircache") as dircache:
+        checkout_dir = dircache + "/sha256/" + dircache_sha256
+        dir_builder = DirectoryBuilder(
+            storage,
+            checkout_dir.encode(),
+            revision["directory"],
+        )
+        dir_builder.build()
+
+        assert os.path.isdir(checkout_dir)
+
+        with tempfile.TemporaryDirectory(suffix="_dest_dirdb") as dirdb:
+            with tempfile.NamedTemporaryFile(suffix=".tar.gz") as dirdb_fd:
+                dirdb_fd.write(tar_metadata[b"disarchive"][b"disarchive_db"])
+                dirdb_fd.flush()
+                proc = subprocess.run(
+                    ["tar", "-xf", dirdb_fd.name],
+                    cwd=dirdb,
+                    capture_output=True,
+                )
+                assert proc.returncode == 0, proc
+
+            proc = subprocess.run(
+                ["disarchive", "load", source_sha256, target_path],
+                env={
+                    **os.environ,
+                    "DISARCHIVE_DB": dirdb,
+                    "DISARCHIVE_DIRCACHE": dircache,
+                },
+                capture_output=True,
+            )
+
+    if proc.returncode != 0:
+        print(proc.stdout.decode())
+        print(proc.stderr.decode())
+    return proc.returncode == 0
+
+
 def check_files_equal(source_path, target_path):
     with open(source_path, "rb") as source_fd, open(target_path, "rb") as target_fd:
         while True:
@@ -233,7 +344,9 @@ def check_files_equal(source_path, target_path):
                 return True
 
 
-def run_one(source_path: str, target_path: Optional[str] = None, *, verbose: bool):
+def run_one(
+    method: str, source_path: str, target_path: Optional[str] = None, *, verbose: bool
+):
     storage = get_storage("memory")
 
     if not os.path.isfile(source_path):
@@ -249,14 +362,16 @@ def run_one(source_path: str, target_path: Optional[str] = None, *, verbose: boo
     if verbose:
         print(f"{source_path}: ingesting")
 
-    (revision_swhid, delta_size) = ingest_tarball(storage, source_path, verbose=verbose)
+    (revision_swhid, delta_size) = ingest_tarball(
+        method, storage, source_path, verbose=verbose
+    )
     if revision_swhid is None:
         print(f"{source_path} could not be loaded")
         return (True, None)
 
     if verbose:
         print(f"{source_path}: reproducing")
-    tar_metadata = get_tar_metadata(storage, revision_swhid)
+    tar_metadata = get_tar_metadata(method, storage, revision_swhid)
     original_filename = tar_metadata[b"filename"]
 
     if target_path is None:
@@ -301,10 +416,13 @@ def cli():
     "--diffoscope", default=False, is_flag=True, help="Run diffoscope before exiting."
 )
 @click.option("--verbose", is_flag=True, help="Verbose mode")
+@click.option(
+    "--method", type=click.Choice(["pristine", "disarchive"]), default="pristine"
+)
 @click.argument("source_path")
 @click.argument("target_path", default="")
-def single(diffoscope, source_path, target_path, verbose):
-    target_file = run_one(source_path, target_path or None, verbose=verbose)
+def single(diffoscope, source_path, target_path, verbose, method):
+    target_file = run_one(method, source_path, target_path or None, verbose=verbose)
 
     if diffoscope:
         if not target_path:
@@ -315,12 +433,15 @@ def single(diffoscope, source_path, target_path, verbose):
 
 
 @cli.command()
+@click.option(
+    "--method", type=click.Choice(["pristine", "disarchive"]), default="pristine"
+)
 @click.argument("source_path")
 @click.argument("target_dir")
-def checkout(source_path, target_dir):
+def checkout(source_path, target_dir, method):
     """Loads the source_path into the storage, then extracts it into the target_dir."""
     storage = get_storage("memory")
-    (revision_swhid, _) = ingest_tarball(storage, source_path, verbose=False)
+    (revision_swhid, _) = ingest_tarball(method, storage, source_path, verbose=False)
     revision_id = hash_to_bytes(revision_swhid.object_id)
     revision = list(storage.revision_get([revision_id]))[0]
 
@@ -338,13 +459,16 @@ def checkout(source_path, target_dir):
 @click.argument("source_paths", nargs=-1)
 @click.option("--verbose", is_flag=True, help="Verbose mode")
 @click.option(
+    "--method", type=click.Choice(["pristine", "disarchive"]), default="pristine"
+)
+@click.option(
     "--fail-early", is_flag=True, help="Stop after the first unreproducible tarball"
 )
-def many(source_paths, verbose, fail_early):
+def many(source_paths, verbose, method, fail_early):
     success = True
 
     for source_path in source_paths:
-        (reproducible, _) = run_one(source_path, verbose=verbose)
+        (reproducible, _) = run_one(method, source_path, verbose=verbose)
         success = success and reproducible
         if fail_early and not reproducible:
             break
