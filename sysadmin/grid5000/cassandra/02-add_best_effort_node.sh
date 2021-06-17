25ae125e37ea2a2ca72b7e401dbf16c764519837
Vincent SELLIER <vincent.sellier@softwareheritage.org>
2021-06-17 10:25:24 +0200

    grid5000/cassandra: Segment paravance cluster nodes
    <10 -> monitoring
    >=10 -> swhstorage
    
    Related to T3357

--- sysadmin/grid5000/cassandra/02-add_best_effort_node.sh
+++ sysadmin/grid5000/cassandra/02-add_best_effort_node.sh
@@ -6,4 +6,10 @@ SCRIPT_DIR="$(pwd $(dirname @0))"
 
 source "${SCRIPT_DIR}/environment.cfg"
 
-oarsub -l "{cluster='${BEST_EFFORT_CLUSTER}'}/nodes=1,walltime=12" -t deploy -t besteffort ${SCRIPT_DIR}/03-deploy_besteffort_nodes.sh
+EXCLUDED_HOSTS=""
+
+for h in ${BEST_EFFORT_EXCLUDED_NODES}; do
+  EXCLUDED_HOSTS="${EXCLUDED_HOSTS},'${h}.${G5K_SITE}'"
+done
+EXCLUDED_HOSTS="$(echo ${EXCLUDED_HOSTS} | cut -b2-)"
+oarsub -l "{cluster='${BEST_EFFORT_CLUSTER}' and host not in (${EXCLUDED_HOSTS})}/nodes=1,walltime=12" -t deploy -t besteffort ${SCRIPT_DIR}/03-deploy_besteffort_nodes.sh
