19515afeb0747a250cdc45359d86cae46e1ef4d5
Vincent SELLIER <vincent.sellier@softwareheritage.org>
2021-08-18 09:32:08 +0200

    grid5000/cassadra: count best_effort jobs in waiting/launching state
    Related to T3357

--- sysadmin/grid5000/cassandra/01-run_best_effort_nodes.sh
+++ sysadmin/grid5000/cassandra/01-run_best_effort_nodes.sh
@@ -8,7 +8,7 @@ NODE_COUNT=4
 while true
 do
         date
-        COUNT="$(oarstat -u | grep -c 'R besteffort')"
+        COUNT="$(oarstat -u | grep -c '[R|W|L] besteffort')"
         if [ $COUNT -lt $NODE_COUNT ]; then
                 ${SCRIPT_DIR}/02-add_best_effort_node.sh
         else
