d4b4d44f1e6170740bbdb13cd00accdc1001ed5a
Vincent SELLIER <vincent.sellier@softwareheritage.org>
2021-09-17 13:53:21 +0200

    grid5000/cassandra: allow to instantiate a best_effort node without an exclusion list
    Related to T3357

--- sysadmin/grid5000/cassandra/02-add_best_effort_node.sh
+++ sysadmin/grid5000/cassandra/02-add_best_effort_node.sh
@@ -8,10 +8,14 @@ source "${SCRIPT_DIR}/environment.cfg"
 
 EXCLUDED_HOSTS=""
 
-TO_EXCLUDE="$(echo ${BEST_EFFORT_EXCLUDED_NODES} | sed 's/,/ /g')"
-
+if [ -n "${EXCLUDED_HOSTS}" ]; then
+  TO_EXCLUDE=" and host not in ($(echo ${BEST_EFFORT_EXCLUDED_NODES} | sed 's/,/ /g'))"
+else
+  TO_EXCLUDE=""
+fi
+  
 for h in ${TO_EXCLUDE}; do
   EXCLUDED_HOSTS="${EXCLUDED_HOSTS},'${h}.${G5K_SITE}'"
 done
-EXCLUDED_HOSTS="$(echo ${EXCLUDED_HOSTS} | cut -b2-)"
-oarsub -l "{cluster='${BEST_EFFORT_CLUSTER}' and host not in (${EXCLUDED_HOSTS})}/nodes=1,walltime=12" -t deploy -t besteffort ${SCRIPT_DIR}/03-deploy_besteffort_nodes.sh
+
+oarsub -l "{cluster='${BEST_EFFORT_CLUSTER}' ${TO_EXCLUDE}}/nodes=1,walltime=12" -t deploy -t besteffort ${SCRIPT_DIR}/03-deploy_besteffort_nodes.sh
