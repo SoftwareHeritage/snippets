2f435c745c6a236b456feb0030903d41280ff9ff
Valentin Lorentz <vlorentz@softwareheritage.org>
2022-03-15 11:59:34 +0100

    recheck_consistency: Add copyright header

--- vlorentz/recheck_consistency.py
+++ vlorentz/recheck_consistency.py
@@ -1,3 +1,8 @@
+# Copyright (C) 2021-2022  The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+
 """
 Reads objects from Kafka or Postgresql, and dumps recovered objects in
 :file:`analyze_consistency_failures/`
