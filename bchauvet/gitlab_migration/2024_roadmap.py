51c653a4d5cd7f8977682f17a1c199e3940fc427
Benoit Chauvet <contact@benoitchauvet.com>
2024-02-13 17:36:32 +0100

    roadmap 24 updates

--- bchauvet/gitlab_migration/2024_roadmap.py
+++ bchauvet/gitlab_migration/2024_roadmap.py
@@ -11,22 +11,29 @@ gl.auth()
 swh_group = gl.groups.get(PM_GROUP)
 
 nb_issues = 0
-issues = swh_group.issues.list(iterator=True)
-
 
 output = open(f"./docs/2024.csv", "w")
 
-output.write(f"|issue|project|roadmap2024|\n")
+output.write(f"issue|project|roadmap2024|url|\n")
 
+projects = swh_group.projects.list(iterator=True)
 
-for issue in issues:
-    project = gl.projects.get(issue.project_id)
-    r24 = 'n'
-    if 'roadmap 2024' in issue.labels:
-        r24='y'
-    output.write(f"{project.name}|{issue.title}|{r24}\n")
-    print(issue.title, ' : ', issue.labels, ' - priority:', '\n')
-    nb_issues+=1
+for gproject in projects:
+    project = gl.projects.get(gproject.id)
+
+    # type= issue
+    issues = project.issues.list(iterator=True, type='ISSUE')
+
+    for issue in issues:
+        if issue.type == 'ISSUE':
+            r24 = 'n'
+            if 'roadmap 2024' in issue.labels:
+                r24='y'
+            output.write(f"{project.name}|{issue.title}|{r24}|{issue.web_url}\n")
+            print(issue.title, ' : ', issue.labels)
+            nb_issues+=1
+            print(issue)
+
 
 print(nb_issues, " issues")
 
