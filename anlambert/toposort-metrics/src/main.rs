ba2c321c05e6c50d0423c5c310ad262c7bcbe9b5
Antoine Lambert <anlambert@softwareheritage.org>
2025-03-03 17:30:46 +0100

    toposort-metrics: Cleanup and improve code

--- anlambert/toposort-metrics/src/main.rs
+++ anlambert/toposort-metrics/src/main.rs
@@ -1,19 +1,24 @@
 use anyhow::{Context, Result};
 use clap::Parser;
-use postgres::{Client, NoTls};
 use std::collections::HashMap;
 use std::path::PathBuf;
+use std::time::Instant;
+
+use human_bytes::human_bytes;
 
 use swh_graph::graph::*;
 use swh_graph::mph::DynMphf;
-use swh_graph::{NodeType, SWHID};
+use swh_graph::NodeType;
 
 use swh_graph_topology::generations::GenerationsReader;
 
 #[derive(Parser, Debug)]
 #[command()]
-/// Reads a topological order .bitstream file, and streams it as a CSV with header
-/// "swhid,generation"
+/// Reads a backward topological order .bitstream file, and computes:
+/// 
+/// - sizes of all directories in a linear pass thanks to that order
+/// 
+/// - total content objects size
 struct Args {
     graph_path: PathBuf,
     #[arg(long)]
@@ -22,62 +27,34 @@ struct Args {
     order: PathBuf,
 }
 
-fn write_dir_length(
-    client: &mut Client,
-    dir_swhid: SWHID,
-    length: u64,
-) -> Result<(), Box<dyn std::error::Error>> {
-    client.execute(
-        "INSERT INTO toposort_metrics (swhid, length) VALUES ($1, $2) ON CONFLICT DO UPDATE",
-        &[&dir_swhid.to_string(), &(length as i64)],
-    )?;
-    Ok(())
-}
-
-fn read_dir_length(
-    client: &mut Client,
-    dir_swhid: SWHID,
-) -> Result<u64, Box<dyn std::error::Error>> {
-    let row = client.query_one(
-        "SELECT length FROM toposort_metrics WHERE swhid = $1",
-        &[&dir_swhid.to_string()],
-    )?;
-    let length: i64 = row.get("length");
-    Ok(length as u64)
-}
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
-    // let mut client = Client::connect("host=localhost user=anlambert password=toto", NoTls)?;
-    // client.batch_execute(
-    //     "
-    // CREATE TABLE IF NOT EXISTS toposort_metrics (
-    //     swhid   TEXT NOT NULL PRIMARY KEY,
-    //     length  BIGINT
-    // )
-    // ",
-    // )?;
 
     log::info!("Loading graph");
     let graph = swh_graph::graph::SwhUnidirectionalGraph::new(args.graph_path)
         .context("Could not load graph")?
-        .load_labels()
-        .context("Could not load labels")?
         .init_properties()
         .load_properties(|props| props.load_maps::<DynMphf>())
         .context("Could not load maps")?
         .load_properties(|props| props.load_contents())
         .context("Could not load content properties")?;
 
-    log::info!("Graph loaded: {} nodes / {} edges", graph.num_nodes(), graph.num_arcs());
+    log::info!(
+        "Graph loaded: {} nodes / {} edges",
+        graph.num_nodes(),
+        graph.num_arcs()
+    );
 
     log::info!("Loading order");
     let reader = GenerationsReader::new(args.order).context("Could not load topological order")?;
 
     log::info!("Computing graph metrics");
 
+    let now = Instant::now();
+
     let mut dir_lengths = HashMap::new();
+    let mut total_contents_size = 0;
 
     for (_, node) in reader
         .iter_nodes()
@@ -90,32 +67,44 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             for succ in graph.successors(node) {
                 let succ_swhid = graph.properties().swhid(succ);
                 if graph.properties().node_type(succ) == NodeType::Directory {
-                    // length += read_dir_length(&mut client, succ_swhid)?;
                     let dir_length = dir_lengths
                         .get(&succ_swhid.to_string())
                         .expect("Invalid topological ordering");
                     length += dir_length;
                 } else if graph.properties().node_type(succ) == NodeType::Content {
-                    length += graph
+                    let size = graph
                         .properties()
                         .content_length(succ)
                         .expect("Missing content length");
+                    let skipped_content = graph.properties().is_skipped_content(succ);
+                    if !skipped_content {
+                        length += size;
+                    }
                 }
             }
-            // write_dir_length(&mut client, dir_swhid, length)?;
             dir_lengths.insert(dir_swhid.to_string(), length);
+        } else if graph.properties().node_type(node) == NodeType::Content {
+            let size = graph
+                .properties()
+                .content_length(node)
+                .expect("Missing content length");
+            let skipped_content = graph.properties().is_skipped_content(node);
+            if !skipped_content {
+                total_contents_size += size;
+            }
         };
     }
+    let elapsed = now.elapsed();
+    log::info!("Graph metrics computed in {:.2?}", elapsed);
 
+    // let mut hash_vec: Vec<(&String, &u64)> = dir_lengths.iter().collect();
+    // hash_vec.sort_by(|a, b| a.1.cmp(b.1));
 
-    log::info!("Graph metrics computed");
+    // for (key, value) in &hash_vec {
+    //     println!("{key}: {value}");
+    // }
 
-    let mut hash_vec: Vec<(&String, &u64)> = dir_lengths.iter().collect();
-    hash_vec.sort_by(|a, b| a.1.cmp(b.1));
-
-    for (key, value) in &hash_vec {
-        println!("{key}: {value}");
-    }
+    println!("Total contents size: {}", human_bytes(total_contents_size as f64));
 
     Ok(())
 }
