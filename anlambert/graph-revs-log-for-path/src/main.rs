a47135d6a485738b2143ded745da3ccb222de3ae
Valentin Lorentz <vlorentz@softwareheritage.org>
2025-03-11 14:25:55 +0100

    Use Result<Option<int>> instead of using 0 as a special value for failures
    0 is a valid node id and filename id, so it's incorrect to use it as
    a special value.
    
    We could use usize::MAX instead, but it's more error-prone (easy to
    forget to handle this case) and less idiomatic in Rust.
    
    This also distinguishes the error case (`Err`) and the non-existence case (`None`)

--- anlambert/graph-revs-log-for-path/src/main.rs
+++ anlambert/graph-revs-log-for-path/src/main.rs
@@ -1,4 +1,4 @@
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use clap::Parser;
 use std::collections::BinaryHeap;
 use std::collections::HashSet;
@@ -38,37 +38,31 @@ fn directory_entry_get_by_path<G: SwhFullGraph>(
     graph: &G,
     rev: NodeId,
     path_parts: &[FilenameId],
-) -> NodeId {
+) -> Result<Option<NodeId>> {
     let props = graph.properties();
-    let mut current_dir = 0;
-    for succ in graph.successors(rev) {
-        if props.node_type(succ) == NodeType::Directory {
-            current_dir = succ;
-            break;
-        }
-    }
-    if current_dir == 0 {
-        return 0;
-    }
+    let mut current_dir = graph
+        .successors(rev)
+        .into_iter()
+        .find(|&succ| props.node_type(succ) == NodeType::Directory)
+        .ok_or_else(|| anyhow!("{} has no root directory", graph.properties().swhid(rev)))?;
 
     for &path_part in path_parts {
-        let mut dir_entry_for_name = 0;
+        let mut dir_entry_for_name = None;
         for (succ, labels) in graph.labeled_successors(current_dir) {
             for label in labels {
                 if let EdgeLabel::DirEntry(dentry) = label {
                     if dentry.filename_id() == path_part {
-                        dir_entry_for_name = succ;
+                        dir_entry_for_name = Some(succ);
                     }
                 }
             }
         }
-        if dir_entry_for_name == 0 {
-            return 0;
-        } else {
-            current_dir = dir_entry_for_name;
+        match dir_entry_for_name {
+            None => return Ok(None), // path does not exist
+            Some(dir_entry_for_name) => current_dir = dir_entry_for_name,
         }
     }
-    current_dir
+    Ok(Some(current_dir))
 }
 
 fn process_revision<G: SwhFullGraph>(graph: &G, rev: &NodeId, heap: &mut BinaryHeap<(i64, usize)>) {
@@ -84,8 +78,8 @@ fn process_parent_revisions<G: SwhFullGraph>(
     rev: NodeId,
     path_parts: &[FilenameId],
     heap: &mut BinaryHeap<(i64, usize)>,
-) -> (bool, bool) {
-    let rev_path_id = directory_entry_get_by_path(graph, rev, path_parts);
+) -> Result<(bool, bool)> {
+    let rev_path_id = directory_entry_get_by_path(graph, rev, path_parts)?;
     let mut parents = Vec::new();
     for succ in graph.successors(rev) {
         if graph.properties().node_type(succ) == NodeType::Revision {
@@ -94,25 +88,25 @@ fn process_parent_revisions<G: SwhFullGraph>(
     }
 
     if parents.is_empty() {
-        return (rev_path_id != 0, false);
+        return Ok((rev_path_id.is_some(), false));
     }
 
     let parent_rev_path_ids = parents
         .clone()
         .into_iter()
         .map(|parent| directory_entry_get_by_path(graph, parent, path_parts))
-        .collect::<Vec<usize>>();
+        .collect::<Result<Vec<Option<usize>>, _>>()?;
     let different_path_ids = parent_rev_path_ids
         .clone()
         .into_iter()
         .all(|parent_rev_path_id| parent_rev_path_id != rev_path_id);
 
-    if rev_path_id != 0 {
+    if let Some(rev_path_id) = rev_path_id {
         if parents.len() == 1 {
             process_revision(graph, parents.first().unwrap(), heap);
         } else {
             for i in 0..parents.len() {
-                if different_path_ids || parent_rev_path_ids[i] == rev_path_id {
+                if different_path_ids || parent_rev_path_ids[i] == Some(rev_path_id) {
                     process_revision(graph, &parents[i], heap);
                     if !different_path_ids {
                         break;
@@ -120,20 +114,20 @@ fn process_parent_revisions<G: SwhFullGraph>(
                 }
             }
         }
+        if Some(rev_path_id) != parent_rev_path_ids[0] && different_path_ids {
+            return Ok((
+                true,
+                parent_rev_path_ids
+                    .into_iter()
+                    .all(|parent_rev_path_id| parent_rev_path_id.is_none()),
+            ));
+        }
     } else {
         for parent in parents {
             process_revision(graph, &parent, heap);
         }
     }
-    if rev_path_id != parent_rev_path_ids[0] && different_path_ids {
-        return (
-            true,
-            parent_rev_path_ids
-                .into_iter()
-                .all(|parent_rev_path_id| parent_rev_path_id == 0),
-        );
-    }
-    (false, false)
+    Ok((false, false))
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -175,7 +169,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             continue;
         }
         visited.insert(rev);
-        let (rev_with_path, stop_revs_walk) = process_parent_revisions(&graph, rev, &path_parts, &mut heap);
+        let (rev_with_path, stop_revs_walk) = process_parent_revisions(&graph, rev, &path_parts, &mut heap)?;
         if rev_with_path {
             println!("{}", graph.properties().swhid(rev));
         }
