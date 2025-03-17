6e51d227151f331876f359e503403e56aac4e170
Martin Kirchgessner <martin.kirchgessner@inria.fr>
2025-03-17 11:50:58 +0100

    sorting and diffing TheStack lists, take2

---
+++ martin/the-stack-v2-train-full-files_extractSortedIDs.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+
+"""
+Requires: PyArrow
+
+This turns Parquet files from `the-stack-v2-train-full-files` into smaller Parquet
+file containing only 1 *sorted* column `sha1_git` using one row per file
+from the original dataset.
+Tune constants below before running.
+"""
+from time import perf_counter
+from datetime import datetime
+
+from pathlib import Path
+import pyarrow as pa
+import pyarrow.dataset as ds
+
+base=Path("/bettik/PROJECTS/pr-swh-codecommons/COMMON/the-stack-v2-train-full-files/data")
+
+target_folder = Path(
+    "/bettik/PROJECTS/pr-swh-codecommons/COMMON/the-stack-v2-train-sorted-IDs"
+)
+
+def read_file(input_i:int, sha1s:list[bytes]) -> None:
+    suffix = f"{input_i:05d}-of-00512"
+
+    dataset = ds.dataset(
+        base / f"train-{suffix}.parquet",
+        format="parquet",
+    ).to_table(
+        columns=["files"],
+    )
+    for i in range(len(dataset[0])):
+        files = dataset[0][i]
+        for j in range(len(files)):
+            try:
+                parsed_sha1 = bytes.fromhex(files[j]["blob_id"].as_py())
+            except Exception as e:
+                print(f"in dataset[0][{i}][{j}]: {e}")
+                continue
+            sha1s.append(parsed_sha1)
+
+    return sha1s
+
+if __name__ == "__main__":
+    global_start = perf_counter()
+    print(datetime.now().isoformat(), "started")
+    sha1s = []
+    for i in range(512):
+        start = perf_counter()
+        read_file(i, sha1s)
+        duration = perf_counter() - start
+        print(
+            datetime.now().isoformat(),
+            f"processed {i} in {duration:.2f} seconds",
+        )
+    print(datetime.now().isoformat(), "sorting")
+    sha1s.sort()
+    print(datetime.now().isoformat(), "writing...")
+
+    schema = pa.schema(
+        {
+            "sha1": pa.binary(20),
+        }
+    )
+    target = pa.Table.from_pydict(
+        {
+            "sha1": sha1s,
+        },
+        schema=schema,
+    )
+
+    ds.write_dataset(
+        target,
+        target_folder,
+        format="parquet",
+    )
+
+    print(
+        datetime.now().isoformat(),
+        f"done in {perf_counter() - global_start:.2f} seconds"
+    )
--- martin/the-stack-v2_gen-download_list.py
+++ martin/the-stack-v2_gen-download_list.py
@@ -5,7 +5,7 @@ Requires: PyArrow
 
 This computes a list of sha1 (`blob_id` in bigcode's datasets) that are in
 [the-stack-v2-full](https://huggingface.co/datasets/bigcode/the-stack-v2)
-and not in `the-stack-v2-train-full-files`.
+and not in `the-stack-v2-train-full-files`, in order to create a "to-download" list.
 
 See constants below
 """
@@ -19,35 +19,43 @@ import pyarrow.dataset as ds
 
 # path to parquet files from the-stack-v2-full, ie. list of metadata
 # TODO full version !
-base = Path("/bettik/PROJECTS/pr-swh-codecommons/COMMON/the-stack-v2/data/ABAP")
+base = Path(
+    "/bettik/PROJECTS/pr-swh-codecommons/COMMON/the-stack-v2/data/JSON/train-00063-of-00064.parquet"
+)
 
-# path to parquet files resulting from `the-stack-v2-train-full-files_extractIDs.py`
+# path to parquet files resulting from `the-stack-v2-train-full-files_extractSortedIDs.py`
 minus_extractedIDs = Path(
-    "/bettik/PROJECTS/pr-swh-codecommons/COMMON/the-stack-v2-train-IDs"
+    "/bettik/PROJECTS/pr-swh-codecommons/COMMON/the-stack-v2-train-sorted-IDs"
 )
 
 target_folder = Path(
     "/bettik/PROJECTS/pr-swh-codecommons/COMMON/sha1_to_download"
 )
 
-def read_sort_base():
+def parse(string_scalar):
+    return bytes.fromhex(string_scalar.as_py())
+
+def read_sort_base() -> list[bytes]:
     complete_list = ds.dataset(base, format="parquet").to_table(columns=["blob_id"])
     print(datetime.now().isoformat(), "loaded complete list")
-    sorted = complete_list.sort_by('blob_id')
-    print(datetime.now().isoformat(), "sorted complete list")
-    return sorted
+    base_hashes = []
+    for i in range(len(complete_list)):
+        try:
+            base_hashes.append(parse(complete_list[i]["blob_id"]))
+        except Exception as e:
+            print(f"in complete_list[{i}]: {e}")
+            continue
+    print(datetime.now().isoformat(), " - parsed")
+    base_hashes.sort()
+    print(datetime.now().isoformat(), " - sorted")
+    return base_hashes
 
 def read_sort_minus():
     minus_list = ds.dataset(minus_extractedIDs, format="parquet").to_table(
         columns=["sha1"]
     )
-    print(datetime.now().isoformat(), "loaded minus list")
-    sorted_minus = minus_list.sort_by("sha1")
-    print(datetime.now().isoformat(), "sorted minus list")
-    return sorted_minus
-
-def parse(string_scalar):
-    return bytes.fromhex(string_scalar.as_py())
+    print(datetime.now().isoformat(), "loaded sorted  minus list")
+    return minus_list
 
 def diff(sorted_base, sorted_minus):
     print(datetime.now().isoformat(), "computing diff")
@@ -55,24 +63,24 @@ def diff(sorted_base, sorted_minus):
     max_minus = len(sorted_minus)
     percent = max_minus // 100
     i_base = 0
-    parsed_base = parse(sorted_base[0][i_base])
     i_minus = 0
+    parsed_minus = sorted_minus[0][i_minus].as_py()
     diff = []
     while i_base < max_base and i_minus < max_minus:
-        if parsed_base < sorted_minus[0][i_minus].as_py():
-            diff.append(parsed_base)
+        if sorted_base[i_base] < parsed_minus:
+            diff.append(sorted_base[i_base])
             i_base += 1
-            parsed_base = parse(sorted_base[0][i_base])
-        elif parsed_base > sorted_minus[0][i_minus].as_py():
+        elif sorted_base[i_base] > parsed_minus:
             # should not happen... but we don't care because `minus` is the dataset
-            # with content included so we will have this content the other way
+            # with content included so we won't miss that content anyway.
             i_minus += 1
+            parsed_minus = sorted_minus[0][i_minus].as_py()
         else:
             i_base += 1
-            parsed_base = parse(sorted_base[0][i_base])
             i_minus += 1
-        if i_minus % percent == 0:
-            print(datetime.now().isoformat(), f"processed {(i_minus*100/max_minus)}% rows")
+            parsed_minus = sorted_minus[0][i_minus].as_py()
+        if i_minus % percent == 1:
+            print(datetime.now().isoformat(), f"processed {(i_minus*100/max_minus):.2f}% rows")
     return diff
 
 if __name__ == "__main__":
@@ -80,9 +88,9 @@ if __name__ == "__main__":
     print(datetime.now().isoformat(), "starting")
 
     sorted_minus = read_sort_minus()
-
     sorted_base = read_sort_base()
     result = diff(sorted_base, sorted_minus)
+
     print(datetime.now().isoformat(), f"diff computed, contains {len(result)} hashes")
 
     schema = pa.schema(
