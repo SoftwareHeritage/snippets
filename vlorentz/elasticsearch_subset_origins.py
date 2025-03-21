1ee42815381946f37a5a503064dcb3a29dd92945
Valentin Lorentz <vlorentz@softwareheritage.org>
2025-03-21 16:39:23 +0100

    elasticsearch_subset_origins.py: Match exact strings for language (to exclude C++, C#, etc. from search for C code)

--- vlorentz/elasticsearch_subset_origins.py
+++ vlorentz/elasticsearch_subset_origins.py
@@ -18,7 +18,8 @@ query = {
     # only return URL and number of stars
     "fields": [
         "url",
-        "jsonld.https://www.w3.org/ns/activitystreams#likes.https://www.w3.org/ns/activitystreams#totalItems.@value",
+        "jsonld." + LIKES_PATH,
+        "jsonld." + LANGUAGE_PATH,
     ],
     "_source": False,
     # filter so that:
@@ -83,6 +84,10 @@ for result in elasticsearch.helpers.scan(client, index="origin-v0.11", query=que
     # result = {'_index': 'origin-v0.11', '_type': '_doc', '_id': '100c7d1725d256d0df485dcacaba5e31dc6db00f', '_score': None, 'fields': {'jsonld': [{'https://www.w3.org/ns/activitystreams#likes.https://www.w3.org/ns/activitystreams#totalItems.@value': ['2']}], 'url': ['https://github.com/alexdimitrov2000/AspNetCore_PersonalProject']}, 'sort': [237142]}
     (url,) = result["fields"]["url"]
     (jsonld,) = result["fields"]["jsonld"]
+    (language,) = jsonld[LANGUAGE_PATH]
     (num_likes,) = jsonld[LIKES_PATH]
+    if language == "C":
+        # TODO: find a way to make Elasticsearch return only exact matches...
+        continue
     writer.writerow(("swh:1:ori:" + result['_id'], url, num_likes))
 
