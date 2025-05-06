beb5513da02b2f23dfd3b7d06d15fc6969bbc265
Antoine Lambert <anlambert@softwareheritage.org>
2025-05-06 11:35:08 +0200

    Improve script to list upstream source URLs of archlinux packages
    Add an option to clone package repositories with git to process
    source info instead of getting the .SRCINFO and PKGBUILD files
    from GitLab API.
    
    Add an option to only process packages whose name is greater than
    a provided string.
    
    Prevent traceback when all packages have been processed.

--- anlambert/list_archlinux_upstream_sources.py
+++ anlambert/list_archlinux_upstream_sources.py
@@ -5,23 +5,30 @@
 # License: GNU General Public License version 3, or any later version
 # See top-level LICENSE file for more information
 
-# Simple script to identify bogus extids associated to hg-checkout visits
-# as associated loader was archiving .hg folders after first deployment
-
 import json
-import os
-import string
-
 import logging
+import os
 import re
-import requests
+import shutil
+import string
 import subprocess
 import tempfile
 
+import click
+import requests
+from dulwich.errors import GitProtocolError
+from dulwich.porcelain import clone
 from srcinfo.parse import parse_srcinfo
+from swh.core.retry import (
+    MAX_NUMBER_ATTEMPTS,
+    WAIT_EXP_BASE,
+    http_retry,
+    retry_if_exception,
+)
+from tenacity import retry as tenacity_retry
 from tenacity.before_sleep import before_sleep_log
-
-from swh.core.retry import http_retry
+from tenacity.stop import stop_after_attempt
+from tenacity.wait import wait_exponential
 
 ARCHLINUX_GITLAB_API_URL = "https://gitlab.archlinux.org/api/v4"
 
@@ -49,28 +56,21 @@ def url_get(url):
     return response
 
 
-# Iterate on all archlinux official packages, extract upstream
-# sources information and dump NDJSON to stdout
-
-page_response = url_get(ARCHLINUX_LIST_PACKAGES_URL)
-packages = page_response.json()
-
-while packages:
-    for package in packages:
-        srcinfo_url = ARCHLINUX_PACKAGE_FILE_URL.substitute(
-            project_id=package["id"], file=".SRCINFO", ref="HEAD"
+def get_package_srcinfo_from_gitlab_api(package):
+    srcinfo_url = ARCHLINUX_PACKAGE_FILE_URL.substitute(
+        project_id=package["id"], file=".SRCINFO", ref="HEAD"
+    )
+    try:
+        srcinfo_src = url_get(srcinfo_url).text
+    except requests.HTTPError:
+        pkgbuild_url = ARCHLINUX_PACKAGE_FILE_URL.substitute(
+            project_id=package["id"], file="PKGBUILD", ref="HEAD"
         )
         try:
-            srcinfo_src = url_get(srcinfo_url).text
+            pkgbuild_response = url_get(pkgbuild_url)
         except requests.HTTPError:
-            pkgbuild_url = ARCHLINUX_PACKAGE_FILE_URL.substitute(
-                project_id=package["id"], file="PKGBUILD", ref="HEAD"
-            )
-            try:
-                pkgbuild_response = url_get(pkgbuild_url)
-            except requests.HTTPError:
-                continue
-
+            srcinfo_src = ""
+        else:
             with tempfile.TemporaryDirectory() as tmpdir:
                 pkgbuild_path = os.path.join(tmpdir, "PKGBUILD")
                 with open(pkgbuild_path, "w") as pkgbuild:
@@ -87,19 +87,111 @@ while packages:
                 srcinfo_src = subprocess.check_output(
                     ["makepkg", "--printsrcinfo"], cwd=tmpdir
                 ).decode()
+    return srcinfo_src
 
-        srcinfo = parse_srcinfo(srcinfo_src)[0]
-        if "source" not in srcinfo:
-            continue
 
-        package_srcinfo = {
-            "origin_url": package["web_url"],
-            "source": srcinfo["source"],
-        }
-        for checksums in ("sha256sums", "sha512sums", "b2sums"):
-            if checksums in srcinfo:
-                package_srcinfo[checksums] = srcinfo[checksums]
-        print(json.dumps(package_srcinfo))
+def is_git_throttling_exception(exc):
+    return isinstance(exc, GitProtocolError) and "unexpected http resp 429" in str(exc)
 
-    page_response = url_get(page_response.links["next"]["url"])
+
+def git_retry(
+    retry=lambda retry_state: retry_if_exception(
+        retry_state, is_git_throttling_exception
+    ),
+    wait=wait_exponential(exp_base=WAIT_EXP_BASE),
+    stop=stop_after_attempt(max_attempt_number=MAX_NUMBER_ATTEMPTS),
+    **retry_args,
+):
+    return tenacity_retry(retry=retry, wait=wait, stop=stop, reraise=True, **retry_args)
+
+
+@git_retry(before_sleep=before_sleep_log(logger, logging.WARNING))
+def clone_repository(repo_url, target_dir):
+    shutil.rmtree(os.path.join(target_dir, ".git"), ignore_errors=True)
+    clone(repo_url, target_dir)
+
+
+def get_package_srcinfo_from_git(package):
+    with tempfile.TemporaryDirectory() as tmpdir:
+        try:
+            clone_repository(package["http_url_to_repo"], tmpdir)
+        except ValueError:
+            # empty repository
+            return ""
+        srcinfo_path = os.path.join(tmpdir, ".SRCINFO")
+        pkgbuild_path = os.path.join(tmpdir, "PKGBUILD")
+        if os.path.exists(srcinfo_path):
+            with open(srcinfo_path, "r") as srcinfo_src:
+                return srcinfo_src.read()
+        elif os.path.exists(pkgbuild_path):
+            # requires pacman-package-manager package installed on debian
+            return subprocess.check_output(
+                ["makepkg", "--printsrcinfo"], cwd=tmpdir, text=True, encoding="utf-8"
+            )
+        else:
+            return ""
+
+
+@click.command()
+@click.option(
+    "--srcinfo-from-git",
+    "-g",
+    default=False,
+    is_flag=True,
+    show_default=True,
+    help=(
+        "Clone each ArchLinux package repository to process source info "
+        "instead of fetching .SRCINFO and PKGBUILD files from GitLab API"
+    ),
+)
+@click.option(
+    "--start-after-name",
+    "-a",
+    default="",
+    show_default=True,
+    help=("Only process packages whose name is greater than the provided string"),
+)
+def run(srcinfo_from_git, start_after_name):
+    """Iterate on all ArchLinux official packages, extract upstream
+    sources information and dump NDJSON to stdout."""
+
+    page_response = url_get(ARCHLINUX_LIST_PACKAGES_URL)
     packages = page_response.json()
+
+    while packages:
+        for package in packages:
+            if package["name"] < start_after_name:
+                continue
+            if srcinfo_from_git:
+                srcinfo_src = get_package_srcinfo_from_git(package)
+            else:
+                srcinfo_src = get_package_srcinfo_from_gitlab_api(package)
+
+            if not srcinfo_src:
+                continue
+
+            srcinfo = parse_srcinfo(srcinfo_src)[0]
+            if "source" not in srcinfo:
+                continue
+
+            package_srcinfo = {
+                "origin_url": package["web_url"],
+                "source": srcinfo["source"],
+            }
+            # recipes for computing source checksums according to its type (bzr, hg,
+            # git or file) can be found in the pacman repository:
+            # https://gitlab.archlinux.org/pacman/pacman/-/tree/master/scripts/libmakepkg/source
+            for checksums in ("sha256sums", "sha512sums", "b2sums"):
+                if checksums in srcinfo:
+                    package_srcinfo[checksums] = srcinfo[checksums]
+            print(json.dumps(package_srcinfo))
+
+        if "next" in page_response.links:
+            page_response = url_get(page_response.links["next"]["url"])
+            packages = page_response.json()
+        else:
+            packages = None
+
+
+if __name__ == "__main__":
+    run()
