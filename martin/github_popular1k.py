8d60f9c81659c5ea37ca3f40ebe1711a339b9a50
Martin Kirchgessner <martin.kirchgessner@inria.fr>
2025-07-04 15:29:34 +0200

    a correct github_popular1k

--- martin/github_popular1k.py
+++ martin/github_popular1k.py
@@ -1,45 +1,66 @@
 #!/usr/bin/env python
 
 """
-Generate the 1000 most starred repositories from Gtihub's GraphQL results.
-Generate "popular1k.json" by
- * opening the GraphQL explorer https://docs.github.com/fr/graphql/overview/explorer
- * log in and authorize
- * enter the query below
- * copy-paste the resulting JSON
- * start this script, redirect it somewhere.
+Generate the 1000 most starred repositories from Github's GraphQL results.
 
-the query (source: https://www.reddit.com/r/github/comments/1hx7iej/comment/m67yp2o/)
+Needs a token in the GITHUB_TOKEN env variable. If you don't have one yet:
+ * create a classic access token https://github.com/settings/tokens/new
+ * authorize public_repo
 
-    query {
-        search(query: "stars:>10000", type: REPOSITORY, first: 100) {
+hints:
+ * initial query source: https://www.reddit.com/r/github/comments/1hx7iej/comment/m67yp2o/
+ * GraphQL explorer https://docs.github.com/fr/graphql/overview/explorer
+ * https://docs.github.com/fr/graphql/guides/using-pagination-in-the-graphql-api
+"""
+
+from json import dumps
+from hashlib import sha1
+from os import environ
+
+import requests
+
+TOKEN = environ["GITHUB_TOKEN"]
+
+def post(end_cursor=None):
+    after = ''
+    if end_cursor:
+        after = f', after: "{end_cursor}"'
+    query = """query {
+        search(query: "stars:>10000", type: REPOSITORY, first: 100 %s) {
             repositoryCount
             edges {
                 node {
                     ... on Repository {
-                        name
-                        owner {
-                            login
-                        }
                         stargazerCount
-                        createdAt
                         url
                     }
                 }
             }
+            pageInfo {
+                endCursor
+                hasNextPage
+            }
         }
-    }
-"""
+    }""" % (after)
+    res = requests.post("https://api.github.com/graphql",
+        headers= {"Authorization": f"Bearer {TOKEN}"},
+        data=dumps({"query": query}),
+    )
+    data = res.json()
+    # print(data)
+    return data
 
-from hashlib import sha1
-from json import loads
-
-with open("popular1k.json") as input:
-    data = loads(input.read())
-    repos = []
-    for r in data["data"]["search"]["edges"]:
+repos = []
+data = post()
+while data:
+    search = data["data"]["search"]
+    for r in search["edges"]:
         repo = r["node"]
         repos.append((repo["stargazerCount"], repo["url"]))
+    if search["pageInfo"]["hasNextPage"]:
+        data = post(end_cursor=search["pageInfo"]["endCursor"])
+    else:
+        data = None
 
 repos.sort(key=lambda r: r[0], reverse=True)
 for r in repos[0:1000]:
