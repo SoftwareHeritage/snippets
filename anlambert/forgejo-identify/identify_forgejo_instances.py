a928fd09cb41963cd5de6012be08519a956e2cd8
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2026-02-11 16:27:31 +0100

    forgejo-identify: Add sql migration generation part
    Refs. swh/infra/sysadm-environment#5777

--- anlambert/forgejo-identify/identify_forgejo_instances.py
+++ anlambert/forgejo-identify/identify_forgejo_instances.py
@@ -17,8 +17,24 @@
 # cat gitea-forges.production.txt | \
 #   ./identify_forgejo_instances.py --environment production
 
+# This can also generate the necessary update queries to execute in both scheduler or
+# web db (in transaction for further checks prior to actually commit those).
+
+# Usage: identify_forgejo_instances.py [OPTIONS]
+# Options:
+#   --force                         Force generation even if generation output
+#                                   already exists
+#   -e, --environment [staging|production]
+#                                   Environment and eponym directory to generate
+#                                   the output data to.
+#   --with-sql-update-scheduler     Flag to generate the db scheduler update sql
+#                                   file.
+#   --with-sql-update-web           Flag to generate the db web update sql file.
+#   --help                          Show this message and exit.
+
 import json
 import os
+import os.path
 import sys
 from io import BytesIO
 from urllib.parse import urlparse
@@ -103,6 +119,12 @@ def fetch_url(url: str, forge_url: str | None = None) -> tuple[str, int]:
 
 @click.command()
 @click.option(
+    "--force",
+    is_flag=True,
+    default=False,
+    help="Force generation even if generation output already exists",
+)
+@click.option(
     "-e",
     "--environment",
     "environment_dir",
@@ -110,77 +132,136 @@ def fetch_url(url: str, forge_url: str | None = None) -> tuple[str, int]:
     type=click.Choice(["staging", "production"]),
     help="Environment and eponym directory to generate the output data to.",
 )
-def main(environment_dir):
+@click.option(
+    "--with-sql-update-scheduler",
+    is_flag=True,
+    default=False,
+    help="Flag to generate the db scheduler update sql file.",
+)
+@click.option(
+    "--with-sql-update-web",
+    is_flag=True,
+    default=False,
+    help="Flag to generate the db web update sql file.",
+)
+def main(environment_dir, force, with_sql_update_scheduler, with_sql_update_web):
     # Create the local environment directory where we will write the output data files
     os.makedirs(environment_dir, exist_ok=True)
 
-    # iterate on forge URLs to disambiguate between gitea and forgejo
-    for forge_url in map(lambda s: s.rstrip("\n/"), sys.stdin):
-        api_url = forge_url + "/api/v1/version"
+    forgejo_urls = set()
+    forgejo_urls_path = environment_dir + "/forgejo_urls"
 
-        # try using forge version from REST API first
-        response, status_code = fetch_url(api_url, forge_url)
+    if force or not os.path.exists(forgejo_urls_path):
+        # iterate on forge URLs to disambiguate between gitea and forgejo
+        for forge_url in map(lambda s: s.rstrip("\n/"), sys.stdin):
+            api_url = forge_url + "/api/v1/version"
 
-        if status_code == 200:
-            try:
-                # gitea version number starts with 1. or v1., can also occasionally
-                # be a short commit hash
-                forge_version = json.loads(response)["version"]
-                if forge_version.startswith(("1.", "v1.")) or "." not in forge_version:
-                    gitea_urls.add(forge_url)
-                else:
+            # try using forge version from REST API first
+            response, status_code = fetch_url(api_url, forge_url)
+
+            if status_code == 200:
+                try:
+                    # gitea version number starts with 1. or v1., can also occasionally
+                    # be a short commit hash
+                    forge_version = json.loads(response)["version"]
+                    if (
+                        forge_version.startswith(("1.", "v1."))
+                        or "." not in forge_version
+                    ):
+                        gitea_urls.add(forge_url)
+                    else:
+                        forgejo_urls.add(forge_url)
+                except Exception:
+                    no_json_response_urls.add(forge_url)
+                    print(
+                        f"Could not decode JSON response from {api_url}",
+                        file=sys.stderr,
+                    )
+            elif status_code == 403:
+                # forge API requires authentication, scrape forge homepage then
+                auth_needed_forge_urls.add(forge_url)
+                response, status_code = fetch_url(forge_url)
+
+                # disambiguate between gitea and forgejo
+                if status_code == 200 and "Powered by Forgejo" in response:
                     forgejo_urls.add(forge_url)
-            except Exception:
-                no_json_response_urls.add(forge_url)
-                print(
-                    f"Could not decode JSON response from {api_url}",
-                    file=sys.stderr,
-                )
-        elif status_code == 403:
-            # forge API requires authentication, scrape forge homepage then
-            auth_needed_forge_urls.add(forge_url)
-            response, status_code = fetch_url(forge_url)
-
-            # disambiguate between gitea and forgejo
-            if status_code == 200 and "Powered by Forgejo" in response:
-                forgejo_urls.add(forge_url)
-            elif status_code == 200 and (
-                "Powered by Gitea" in response or "docs.gitea" in response
-            ):
-                gitea_urls.add(forge_url)
-            elif status_code == 200:
-                print(
-                    f"Could not determine forge type for URL {forge_url}",
-                    file=sys.stderr,
-                )
-            elif status_code != 200:
+                elif status_code == 200 and (
+                    "Powered by Gitea" in response or "docs.gitea" in response
+                ):
+                    gitea_urls.add(forge_url)
+                elif status_code == 200:
+                    print(
+                        f"Could not determine forge type for URL {forge_url}",
+                        file=sys.stderr,
+                    )
+                elif status_code != 200:
+                    http_error_urls.add(f"{forge_url}, {status_code}")
+                    print(
+                        f"Error when fetching {forge_url}: {status_code}",
+                        file=sys.stderr,
+                    )
+            elif status_code:
                 http_error_urls.add(f"{forge_url}, {status_code}")
                 print(
-                    f"Error when fetching {forge_url}: {status_code}",
-                    file=sys.stderr,
+                    f"Error when fetching {forge_url}: {status_code}", file=sys.stderr
                 )
-        elif status_code:
-            http_error_urls.add(f"{forge_url}, {status_code}")
-            print(f"Error when fetching {forge_url}: {status_code}", file=sys.stderr)
 
-    # Start writing the data output files
-    with open(environment_dir + "/forgejo_urls", "w") as f:
-        f.write("\n".join(sorted(forgejo_urls)))
+        # Start writing the data output files
+        with open(forgejo_urls_path, "w") as f:
+            f.write("\n".join(sorted(forgejo_urls)))
 
-    with open(environment_dir + "/gitea_urls", "w") as f:
-        f.write("\n".join(sorted(gitea_urls)))
+        with open(environment_dir + "/gitea_urls", "w") as f:
+            f.write("\n".join(sorted(gitea_urls)))
 
-    with open(environment_dir + "/dead_forge_urls", "w") as f:
-        f.write("\n".join(sorted(dead_urls)))
+        with open(environment_dir + "/dead_forge_urls", "w") as f:
+            f.write("\n".join(sorted(dead_urls)))
 
-    with open(environment_dir + "/http_error_forge_urls", "w") as f:
-        f.write("\n".join(sorted(http_error_urls)))
+        with open(environment_dir + "/http_error_forge_urls", "w") as f:
+            f.write("\n".join(sorted(http_error_urls)))
 
-    with open(environment_dir + "/auth_needed_forge_urls", "w") as f:
-        f.write("\n".join(sorted(auth_needed_forge_urls)))
+        with open(environment_dir + "/auth_needed_forge_urls", "w") as f:
+            f.write("\n".join(sorted(auth_needed_forge_urls)))
 
-    with open(environment_dir + "/anubis_protected_forge_urls", "w") as f:
-        f.write("\n".join(sorted(anubis_protected_forge_urls)))
+        with open(environment_dir + "/anubis_protected_forge_urls", "w") as f:
+            f.write("\n".join(sorted(anubis_protected_forge_urls)))
+
+    # We bypass the generation, let's read the file holding the forgejo urls to migrate
+    if not forgejo_urls and os.path.exists(forgejo_urls_path):
+        with open(forgejo_urls_path, "r") as f:
+            forgejo_urls = set([url.rstrip("\n") for url in f.readlines()])
+
+    if with_sql_update_scheduler:
+        if not forgejo_urls:
+            raise ValueError(f"<{forgejo_urls}> should not be empty!")
+
+        with open(environment_dir + "/sql_update_scheduler.sql", "w") as sql_sched_f:
+            for url in forgejo_urls:
+                instance = urlparse(url).netloc
+                row_sql_update = """
+update listers
+set name='forgejo'
+where name='gitea' and instance_name='%s';
+
+update task set type='list-forgejo-full'
+where type='list-gitea-full' and
+      (arguments#>>'{kwargs,instance}'='%s' or arguments#>>'{kwargs,url}'='%s');
+
+"""
+                sql_sched_f.write(row_sql_update % (instance, instance, url))
+
+    if with_sql_update_web:
+        if not forgejo_urls:
+            raise ValueError(f"<{forgejo_urls}> should not be empty!")
+
+        with open(environment_dir + "/sql_update_web.sql", "w") as sql_web_f:
+            for url in forgejo_urls:
+                row_sql_update = f"""
+update add_forge_request
+set forge_type='forgejo'
+where forge_type='gitea' and forge_url='{url}/';
+
+"""
+                sql_web_f.write(row_sql_update)
 
 
 if __name__ == "__main__":
